46717e53338112de88aeeb0d33da63bddf20ea83
Mohit <mohitdutta@live.com>
2019-10-17 19:59:49 +0530

    Getting Started

---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+    'url' : 'mongodb://localhost:27017/express'
+    //'mongodb://127.1.1.0/express' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+     //Please replace your host file Here : 127.1.1.0 , Express is Collection Name (Database Name)
+};
